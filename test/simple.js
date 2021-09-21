c49981775bd2271fe0aed7f29416370f8f61b811
Andrey Sitnik <andrey@sitnik.ru>
2021-09-21 12:59:33 +0200

    The simplest way to test old Node.js versions

---
+++ test/simple.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+let assets = require('assert')
+
+let { createColors } = require('../')
+
+let { red, isColorSupported } = createColors(true)
+
+assets.ok(isColorSupported)
+assets.equal(red('text'), '\x1B[31mtext\x1B[39m')
