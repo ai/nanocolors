3ae0c86720309f85c21ec1faeb9fb216913d6eed
Andrey Sitnik <andrey@sitnik.ru>
2021-09-21 04:31:52 +0200

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Does environment support color output.
+ *
+ * ```js
+ * import { isColorSupported } from 'nanocolors'
+ *
+ * if (isColorSupported) {
+ *   console.log('With colors')
+ * }
+ * ```
+ */
+export const isColorSupported: boolean
---
+++ index.js
@@ -0,0 +1,3 @@
+const isColorSupported = false
+
+module.exports = { isColorSupported }
