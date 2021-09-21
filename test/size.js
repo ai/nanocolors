e1fef3f12d4e4b80a09fa6c522d5ecc96ea4e95b
Andrey Sitnik <andrey@sitnik.ru>
2021-09-21 07:35:34 +0200

    Add size benchmark

---
+++ test/size.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+let { mkdirSync, rmSync, writeFileSync } = require('fs')
+let { execSync } = require('child_process')
+let { join } = require('path')
+
+let { bold } = require('..')
+
+function getSize(lib) {
+  let testDir = join(__dirname, 'size-test')
+  mkdirSync(testDir)
+  writeFileSync(join(testDir, 'package.json'), '{"private":true}')
+  execSync(`yarn add ${lib}`, { cwd: testDir })
+  let out = execSync(`du -sh node_modules/`, { cwd: testDir }).toString()
+  rmSync(testDir, { recursive: true, force: true })
+  return out.match(/^(\d+)K/)[1]
+}
+
+function benchmark(lib) {
+  process.stdout.write(
+    lib.padEnd('ansi-colors  '.length) +
+      bold(getSize(lib).padStart(4)) +
+      ' KB\n'
+  )
+}
+
+benchmark('chalk')
+benchmark('ansi-colors')
+benchmark('kleur')
+benchmark('colorette')
