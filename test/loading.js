c5f3bc4ddb9476c679b042e6f75b2b280e672ae0
Andrey Sitnik <andrey@sitnik.ru>
2021-09-21 20:01:03 +0200

    Improve loading benchmark output

--- test/loading.js
+++ test/loading.js
@@ -4,6 +4,7 @@ let before
 function showTime(name) {
   let after = performance.now()
   let time = (Math.round(1000 * (after - before)) / 1000).toString()
+  time = time.replace(/\.\d$/, '$&00').replace(/\.\d\d$/, '$&0')
   let title = name.padEnd('kleur/colors  '.length)
   process.stdout.write(title + '\x1B[1m' + time.padStart(6) + '\x1B[22m ms\n')
 }
