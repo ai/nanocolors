55453e52e5937c876bd59314fc78c1d59446e434
Andrey Sitnik <andrey@sitnik.ru>
2021-09-23 07:58:58 +0200

    Even more compress CJS files

--- index.browser.cjs
+++ index.browser.cjs
@@ -1,62 +1,35 @@
 let nope = s => String(s)
 
-let isColorSupported = false
-let reset = nope
-let bold = nope
-let dim = nope
-let italic = nope
-let underline = nope
-let inverse = nope
-let hidden = nope
-let strikethrough = nope
-let black = nope
-let red = nope
-let green = nope
-let yellow = nope
-let blue = nope
-let magenta = nope
-let cyan = nope
-let white = nope
-let gray = nope
-let bgBlack = nope
-let bgRed = nope
-let bgGreen = nope
-let bgYellow = nope
-let bgBlue = nope
-let bgMagenta = nope
-let bgCyan = nope
-let bgWhite = nope
-
 function createColors() {
   return {
     isColorSupported,
-    reset,
-    bold,
-    dim,
-    italic,
-    underline,
-    inverse,
-    hidden,
-    strikethrough,
-    black,
-    red,
-    green,
-    yellow,
-    blue,
-    magenta,
-    cyan,
-    white,
-    gray,
-    bgBlack,
-    bgRed,
-    bgGreen,
-    bgYellow,
-    bgBlue,
-    bgMagenta,
-    bgCyan,
-    bgWhite
+    reset: nope,
+    bold: nope,
+    dim: nope,
+    italic: nope,
+    underline: nope,
+    inverse: nope,
+    hidden: nope,
+    strikethrough: nope,
+    black: nope,
+    red: nope,
+    green: nope,
+    yellow: nope,
+    blue: nope,
+    magenta: nope,
+    cyan: nope,
+    white: nope,
+    gray: nope,
+    bgBlack: nope,
+    bgRed: nope,
+    bgGreen: nope,
+    bgYellow: nope,
+    bgBlue: nope,
+    bgMagenta: nope,
+    bgCyan: nope,
+    bgWhite: nope
   }
 }
 
-module.exports = createColors(isColorSupported)
+module.exports = createColors()
 module.exports.createColors = createColors
