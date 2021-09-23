2d9399a7494cc1f0e24df8e9786f70ea280f440b
Andrey Sitnik <andrey@sitnik.ru>
2021-09-23 08:13:04 +0200

    Show version in size test

--- test/size.js
+++ test/size.js
@@ -5,27 +5,33 @@ import { fileURLToPath } from 'url'
 import { execSync } from 'child_process'
 import { join } from 'path'
 
-import { bold } from '../index.js'
+import { bold, gray } from '../index.js'
 
 function getSize(lib) {
   let testDir = join(fileURLToPath(import.meta.url), '..', 'size-test')
   mkdirSync(testDir)
   writeFileSync(join(testDir, 'package.json'), '{"private":true}')
-  execSync(`yarn add ${lib}`, { cwd: testDir })
+  let install = execSync(`yarn add ${lib}@latest`, { cwd: testDir }).toString()
+  let version = install.match(new RegExp(lib + '@(\\d+\\.\\d+\\.\\d+)'))[1]
   let out = execSync(`du -sh node_modules/`, { cwd: testDir }).toString()
   rmSync(testDir, { recursive: true, force: true })
+  let size
   if (out.includes('M')) {
-    return String(parseFloat(out.match(/^(\d+(,\d+)?)M/)[1]) * 1024)
+    size = String(parseFloat(out.match(/^(\d+(,\d+)?)M/)[1]) * 1024)
   } else {
-    return out.match(/^(\d+)K/)[1]
+    size = out.match(/^(\d+)K/)[1]
   }
+  return [size, version]
 }
 
 function benchmark(lib) {
+  let [size, version] = getSize(lib)
   process.stdout.write(
     lib.padEnd('ansi-colors  '.length) +
-      bold(getSize(lib).padStart(4)) +
-      ' KB\n'
+      bold(size.padStart(4)) +
+      ' KB ' +
+      gray(version) +
+      '\n'
   )
 }
 
